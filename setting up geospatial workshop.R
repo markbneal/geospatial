80b0676645f738d8956cfc3cc3c789ab48009aaa
markbneal <mark.neal@dairynz.co.nz>
2020-08-25 16:55:53 +1200

    Initial commit

---
+++ setting up geospatial workshop.R
@@ -0,0 +1,29 @@
+#https://github.com/rstudio-conf-2020/geospatial
+
+install.packages(c("sf", "tmap", "mapview", "raster", "dplyr"))
+
+install.packages(c("tidycensus", "FedData", "rnaturalearth", "osmdata", "OpenStreetMap", "shinyjs", "rmapshaper"))
+
+install.packages("rJava")
+
+#install.packages("zrsaSpatialWorkshop")
+#library(remotes)
+#install_github("rstudio-conf-2020/geospatial")
+#install.packages("zrsaSpatialWorkshop_0.2.0.tar.gz", repos = NULL, type="source") #this works
+
+library(zrsaSpatialWorkshop)
+open_exercise(1)
+open_solution(1)
+open_exercise(2)
+open_solution(2)
+open_exercise(3)
+open_solution(3)
+open_exercise(4)
+open_solution(4)
+open_exercise(5)
+open_solution(5)
+open_exercise(6)
+open_solution(6)
+open_exercise(7)
+open_solution(7)
+
